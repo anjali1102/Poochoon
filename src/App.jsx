85019cdc99f66146e64a90fe96d5237d72f13485
anjali1102 <ac.chauhan1102@gmail.com>
2022-04-07 20:53:16 +0530

    Feat: added category page

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Settings from './pages/Settings';
+import FinalScore from './pages/FinalScore';
+import './App.css'; 
+import Questions from './pages/Questions';
+import { Container } from '@mui/material';
+import { Box } from '@mui/system';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Container maxWidth='sm'>
+        <Box textAlign='center' mt={5}>
+          <Routes>
+            <Route path='/' element={<Settings />} />
+            <Route path='/questions' element={<Questions />} />
+            <Route path='/score' element={<FinalScore />} />
+          </Routes>
+        </Box>
+      </Container>
+    </Router>
+  );
+}
+
+export default App;

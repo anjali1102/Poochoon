54e3538d1bad3e78596e5e81b42646df2bc6fa0a
anjali1102 <ac.chauhan1102@gmail.com>
2022-04-17 05:06:03 +0530

    Adding question Page

--- src/pages/Questions.jsx
+++ src/pages/Questions.jsx
@@ -1,9 +1,24 @@
+import { Button, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import useAxios from "../hooks/useAxios";
 
 const Questions = () => {
+  let apiUrl = `/api.php?amount=10`;
+  const { response, loading } = useAxios({ url: apiUrl });
+  console.log(response);
   return (
-    <div>
-      Questions
-    </div>);
+    <Box>
+      <Typography variant="h4">Question1</Typography>
+      <Typography mt={5}>This is the Question?</Typography>
+      <Box mt={2}>
+        <Button variant="contained">Answer 1</Button>
+      </Box>
+      <Box mt={2}>
+        <Button variant="contained">Answer 2</Button>
+      </Box>
+      <Box mt={5}>Score: 2/6</Box>
+    </Box>
+  );
 };
 
 export default Questions;
